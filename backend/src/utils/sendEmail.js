800950d5b2240879c8c0e3bf4ad227fe396d6ca9
kapil9562 <adhikarikapil389@gmail.com>
2026-02-20 08:47:57 +0530

    send email fix

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,11 +5,14 @@ dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
   connectionTimeout: 10000,
 });
