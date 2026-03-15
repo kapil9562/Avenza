ebd34965b4938569fe41a5f1dda0fb3b25161848
kapil9562 <adhikarikapil389@gmail.com>
2026-03-15 16:12:46 +0530

    ui updated card size fixed

--- avenzaBackend/src/routes/FavItemsRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { toggleFav, getFavItems } from "../controller/FavItems.controller.js";
-
-const FavItemsRouter = express.Router()
-
-FavItemsRouter.post('/favorite/toggle', toggleFav);
-FavItemsRouter.get('/favorite/getItems', getFavItems);
-
-export {FavItemsRouter}
