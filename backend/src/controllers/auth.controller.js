56ebc4404e6a5a29e4908b4ee1cc30cee6aa641c
kapil9562 <adhikarikapil389@gmail.com>
2026-05-03 19:32:32 +0530

    fixed cookies problem fixed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -26,6 +26,7 @@ const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    domain: ".myavenza.com",
     maxAge: 7 * 24 * 60 * 60 * 1000
 }
 
